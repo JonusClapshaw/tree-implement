49ab706b2f23410b42d2234331472777037fde33
Jonus <joclapshaw@gmail.com>
2026-02-17 14:36:38 -0800

    Day2 of livecode

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,36 +1,69 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10, null, null);
-    root.left = new TreeNode(9, null, null);
-    root.left.left = new TreeNode(5, null, null);
-    root.left.right = new TreeNode(2, null, null);
+    TreeNode<Integer> root = new TreeNode<Integer>(10, null, null);
+    root.left = new TreeNode<Integer>(9, null, null);
+    root.left.left = new TreeNode<Integer>(5, null, null);
+    root.left.right = new TreeNode<Integer>(2, null, null);
 
-    root.right = new TreeNode(15, null, null);
-    root.right.left = new TreeNode(-3, null, null);
-    root.right.right = new TreeNode(5, null, null);
-    root.right.right.right = new TreeNode(22, null, null);
+    root.right = new TreeNode<Integer>(15, null, null);
+    root.right.left = new TreeNode<Integer>(-3, null, null);
+    root.right.right = new TreeNode<Integer>(5, null, null);
+    root.right.right.right = new TreeNode<Integer>(22, null, null);
+
+    TreeNode<String> stringRoot = new TreeNode<String>("Hello", null, null);
+
+    stringRoot.left = new TreeNode<String>("how", null, null);
+    stringRoot.left.left = new TreeNode<String>("are", null, null);
+    stringRoot.left.right = new TreeNode<String>("you", null, null);
+
+    stringRoot.right = new TreeNode<String>("I", null, null);
+    stringRoot.right.left = new TreeNode<String>("am", null, null);
+    stringRoot.right.right = new TreeNode<String>("doing", null, null);
+    stringRoot.right.right.right = new TreeNode<String>("good", null, null);
   
-    preorder(root);
-    postorder(root);
-    centralorder(root);
+    //preorder(root);
+    //postorder(root);
+    //centralorder(stringRoot);
+
+    //printGreater(root, 10);
+    System.out.println(countNodes(stringRoot));
+  }
+
+  public static void printGreater(TreeNode<Integer> node, int threshold) {
+    if(node == null) return;
+    if(node.value > threshold){
+      System.out.println(node.value);
+    }
+    printGreater(node.left, threshold);
+    printGreater(node.right, threshold);
+  }
+
+  public static int countNodes(TreeNode<?> node){
+    return node == null ? 0 : countNodes(node.left) + countNodes(node.right) + 1;
+
+    // if(node == null) return 0;
+    // int leftCount = countNodes(node.left);
+    // int rightCount= countNodes(node.right);
+    // int overallCount = leftCount + rightCount + 1;
+    // return overallCount;
   }
 
   // Print a tree rooted at the given node in pre order
-  public static void preorder(TreeNode node){
+  public static void preorder(TreeNode<?> node){
     if(node == null) return;
     System.out.println(node.value);
     preorder(node.left);
     preorder(node.right);
   }
 
-  public static void postorder(TreeNode node){
+  public static <T> void postorder(TreeNode<T> node){
     if(node == null) return;
     postorder(node.left);
     postorder(node.right);
     System.out.println(node.value);
   }
 
-  public static void centralorder(TreeNode node){
+  public static <E> void centralorder(TreeNode<E> node){
     if(node == null) return;
     centralorder(node.left);
     System.out.println(node.value);
